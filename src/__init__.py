b9bc9d3758a4d4a58f5882842a94559823970774
Matt Lane <lane.matthew@gmail.com>
2019-08-02 22:45:50 -0700

    removing starter code from __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,7 @@
-def clean_episode(filepath):
-    chris_lines = []
-    current_chris_section = []
-    with open(filepath) as file:
-        for line in file.readlines():
-            if line.startswith("Chris:   "):
-                current_chris_section.append(line)
-            else:
-                if len(current_chris_section) > 1:
-                    chris_lines.append(current_chris_section)
-                current_chris_section = []
-    from IPython import embed; embed()
+# from models import Conversation
 
 
 if __name__ == '__main__':
-    clean_episode("./episodes/ba_01.txt")
+    pass
+    # lines = clean_episode("./episodes/ba_01.txt")
+    # from IPython import embed; embed()
