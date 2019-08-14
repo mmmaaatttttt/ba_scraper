166ea3914c3b58a68c26897818f91a4262263503
Matt Lane <lane.matthew@gmail.com>
2019-08-13 19:38:40 -0700

    adding truncate

--- ba_scraper/helpers.py
+++ ba_scraper/helpers.py
@@ -27,3 +27,27 @@ def speaker_features(line):
     for bigram in bigram_set:
         features[f"contains_bigram={bigram}"] = True
     return features
+
+
+def truncate(s, num_chars=30):
+    """Truncate a string to the specified number of characters.
+
+    If truncation occurs, include an ellipsis at the end.
+
+    >>> truncate("Hello world", 5)
+    'He...'
+
+    >>> truncate("Hello world")
+    'Hello world'
+
+    >>> truncate("Hello world", 11)
+    'Hello world'
+
+    >>> truncate("Hello world", 10)
+    'Hello w...'
+
+    """
+    if len(s) <= num_chars:
+        return s
+
+    return s[:num_chars - 3] + "..."
