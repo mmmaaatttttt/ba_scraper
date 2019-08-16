52908a20fba0680877c3dbe5e280bd06d5342568
Matt Lane <lane.matthew@gmail.com>
2019-08-16 13:58:55 -0700

    modifying __main__

--- ba_scraper/__main__.py
+++ ba_scraper/__main__.py
@@ -5,4 +5,9 @@ if __name__ == '__main__':
     transcript_names = os.listdir(os.path.join('episodes'))
     convo_list = ConversationList(transcript_names)
     for convo in convo_list.conversations:
-        print(convo.word_count_summary_json())
+        # print(convo.word_count_summary_json())
+        print(convo.sentiment_count_json(-1, -0.5))
+        print(convo.sentiment_count_json(-0.5, -0.05))
+        print(convo.sentiment_count_json(-0.05, 0.05))
+        print(convo.sentiment_count_json(0.05, 0.5))
+        print(convo.sentiment_count_json(0.5, 1))
